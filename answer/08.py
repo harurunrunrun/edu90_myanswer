b163c7e66f4ab9912466ac9e599fb65c358bf87a
harurunrunrun <68173582+harurunrunrun@users.noreply.github.com>
2021-04-11 02:24:48 +0900

    Add files via upload

---
+++ answer/08.py
@@ -0,0 +1,73 @@
+class INPUT:
+  def __init__(self):
+    self.l=open(0).read().split()[::-1]
+    self.length=len(self.l)
+    return
+
+  def stream(self,k=1,f=int,f2=False):
+    assert(-1<k)
+    m=self.length
+    if m==0 or m<k:
+      raise Exception("There is no input!")
+    elif f!=str:
+      if k==0:
+        self.length=0
+        return list(map(f,self.l[::-1]))
+      if k==1 and not f2:
+        self.length-=1
+        return f(self.l.pop())
+      if k==1 and f2:
+        self.length-=1
+        return [f(self.l.pop())]
+      ret=[]
+      for _ in [0]*k:
+        ret.append(f(self.l.pop()))
+      self.length-=k
+      return ret
+    else:
+      if k==0:
+        self.length=0
+        return self.l[::-1]
+      if k==1 and not f2:
+        self.length-=1
+        return self.l.pop()
+      if k==1 and f2:
+        self.length-=1
+        return [self.l.pop()]
+      ret=[]
+      for _ in [0]*k:
+        ret.append(self.l.pop())
+      self.length-=k
+      return ret
+pin=INPUT().stream
+
+"""
+pin(number[default:1],f[default:int],f2[default:False])
+if number==0 -> return left all
+listを変数で受け取るとき、必ずlistをTrueにすること。
+"""
+
+def main():
+  S=pin(1,str)
+  t=len(S)
+  l=["a","t","c","o","d","e","r"]
+  d=[1]*t
+  s=0
+  mod=10**9+7
+  for i in range(6):
+    s=0
+    for j in range(t):
+      if S[j]==l[i]:
+        s+=d[j]%mod
+      elif S[j]==l[i+1]:
+        d[j]=s
+      s%=mod
+  ans=0
+  for i in range(t):
+    if S[i]=="r":
+      ans+=d[i]
+      ans%=mod
+  print(ans)
+  return
+
+main()
